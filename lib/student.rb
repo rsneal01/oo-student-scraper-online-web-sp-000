37b3260cf2a057b6f67582fb90a54e75c1e96a2e
Bobby Neal <rsneal01@gmail.com>
2020-07-30 17:41:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute|
-      attributes_hash[:attribute] = attribute
+      self.attribute = attribute
 
     end
   end
